2e53adc6207d67b04ef2c80a368672afeb829437
attrn <anh.tt@outlook.com>
2022-07-19 12:54:23 +0900

    data_loader now also returns a list of all unique categories

--- data_loader.py
+++ data_loader.py
@@ -1,4 +1,4 @@
-from typing import List, Iterator, NamedTuple
+from typing import List, Tuple, NamedTuple
 from pyparsing import *
 from base import Token, ConstituentNode, Category
 
@@ -9,10 +9,17 @@ class DataItem(NamedTuple):
     tree_root: ConstituentNode
 
 
-def load_auto_file(filename: str) -> Iterator[DataItem]:
+def load_auto_file(filename: str) -> Tuple[List[DataItem], List[str]]:
     """
-    load CCG data from .auto file sentence by sentence
-    returns an iterator
+    load CCG data from .auto file
+
+    Args:
+        filename (str): name of .auto file
+
+    Returns:
+        Tuple[List[DataItem], List[str]]: a tuple that contains:
+            - a list of CCG trees, each corresponding to a sentence
+            - a list of all unique categories in the file
     """
 
     # pyparsing patterns
@@ -32,80 +39,97 @@ def load_auto_file(filename: str) -> Iterator[DataItem]:
 
     parser = OneOrMore(nestedExpr(content=(non_leaf | leaf)))
 
-    for line in open(filename):
-        line = line.strip()
+    with open(filename, 'r') as f:
+        lines = f.readlines()
 
-        if len(line) == 0:
-            continue
-        if line.startswith("ID"):
-            id = line
-        else:
-            tokens = list()
+        data_items = list()
+        all_cats = set()
 
-            try:
-                parsed = parser.parseString(line).asList()
-            except ParseException as e:
-                print('pyparsing error: ' + repr(e))     # TODO: use a logger at some point
-                # raise
+        for line in lines:
+            line = line.strip()
+
+            if len(line) == 0:
                 continue
+            if line.startswith("ID"):
+                id = line
+            else:
+                tokens = list()
 
-            if parsed is None or len(parsed) == 0 or len(parsed[0]) == 0:
-                continue
-            elif parsed[0][0] == 'L':     # if leaf node
-                parsed = parsed[0]
-                category = parsed[1]
-                pos = parsed[2]
-                word = parsed[3]
+                try:
+                    parsed = parser.parseString(line).asList()
+                except ParseException as e:
+                    print('pyparsing error: ' + repr(e))     # TODO: use a logger at some point
+                    # raise
+                    continue
 
-                token = Token(contents=word, POS=pos, tag=Category.parse(category))
-                tokens.append(token)
-            else:                         # if non-leaf node   
-                parsed = parsed[0]
+                if parsed is None or len(parsed) == 0 or len(parsed[0]) == 0:
+                    continue
+                elif parsed[0][0] == 'L':     # if leaf node
+                    parsed = parsed[0]
+                    category = parsed[1]
+                    pos = parsed[2]
+                    word = parsed[3]
 
-                def _traverse(res):
-                    if res is not None and len(res) > 0:
-                        if res[0] == 'L':
-                            category = res[1]
-                            pos = res[2]
-                            word = res[3]
+                    token = Token(contents=word, POS=pos, tag=Category.parse(category))
+                    tokens.append(token)
+                    all_cats.add(category)
+                else:                         # if non-leaf node
+                    parsed = parsed[0]
 
-                            token = Token(contents=word, POS=pos, tag=Category.parse(category))
-                            tokens.append(token)
+                    def _traverse(res):
+                        if res is not None and len(res) > 0:
+                            if res[0] == 'L':
+                                category = res[1]
+                                pos = res[2]
+                                word = res[3]
 
-                            return token
-                        else:
-                            category = res[1]
-                            num_children = res[2]
+                                token = Token(contents=word, POS=pos, tag=Category.parse(category))
+                                tokens.append(token)
+                                all_cats.add(category)
 
-                            node = ConstituentNode(tag=Category.parse(category))
-
-                            if num_children == 1:
-                                children = [_traverse(res[3])]
+                                return token
                             else:
-                                children = [_traverse(res[3]), _traverse(res[4])]
+                                category = res[1]
+                                num_children = res[2]
 
-                            node.children = children
+                                node = ConstituentNode(tag=Category.parse(category))
+                                all_cats.add(category)
 
-                            return node
+                                if num_children == 1:
+                                    children = [_traverse(res[3])]
+                                else:
+                                    children = [_traverse(res[3]), _traverse(res[4])]
 
-                root = _traverse(parsed)
+                                node.children = children
 
-                yield DataItem(id, tokens, root)
+                                return node
+
+                    root = _traverse(parsed)
+
+                    data_items.append(DataItem(id, tokens, root))
+
+        return data_items, list(all_cats)
 
 
 if __name__ == '__main__':
+    # sample usage
     filename = "data/ccg-sample.auto"
 
-    for item in load_auto_file(filename):
+    items, cats = load_auto_file(filename)
+
+    for item in items:
         print(item.id)
 
         for token in item.tokens:
             print('{}\t{}\t{}'.format(token.contents, token.POS, token.tag))
         
         root = item.tree_root
+
         def _iter(node):
             print(node.tag)
             if isinstance(node, ConstituentNode):
                 for child in node.children:
                     _iter(child)
         _iter(root)
+
+    print(cats)
