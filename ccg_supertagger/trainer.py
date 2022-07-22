9c3bd3e5db4f8dfc9d5b8285084bd55a57fb1d7e
lkh23333 <1900011070@pku.edu.cn>
2022-07-22 10:13:12 -0400

    update

--- ccg_supertagger/trainer.py
+++ ccg_supertagger/trainer.py
@@ -1,5 +1,5 @@
 from typing import Union, Optional, Dict, Any
-import sys, os, argparse
+import sys, os, argparse, logging
 import torch
 import torch.nn as nn
 from torch.optim import AdamW
@@ -8,10 +8,12 @@ from torch.utils.data import Dataset, DataLoader
 from transformers import BertTokenizer
 
 from models.simple_model import CCGSupertaggerModel
-from utils import prepare_data, get_cross_entropy_loss
+from utils import prepare_data
 
 sys.path.append('..')
 from data_loader import AutoParser
+
+UNK_CATEGORY = 'UNK_CATEGORY'
 
 
 class CCGSupertaggingDataset(Dataset):
@@ -54,101 +56,120 @@ class CCGSupertaggingTrainer:
         self.batch_size = batch_size
         self.optimizer = optimizer
         self.lr = lr
+        self.criterion = nn.CrossEntropyLoss()
 
-    def train(self, dataset: Dataset):
-        dataloader = DataLoader(
-            dataset = dataset,
+    def train(self, train_dataset: Dataset, dev_dataset: Dataset):
+        train_dataloader = DataLoader(
+            dataset = train_dataset,
             batch_size = self.batch_size,
             shuffle = True,
             collate_fn = collate_fn
         )
 
-        self.model.to(self.device)
+        self.model = self.model.to(self.device)
         self.model.train()
 
         params = filter(lambda p: p.requires_grad, self.model.parameters())
         optimizer = self.optimizer(params, lr = self.lr)
 
         for epoch in range(1, self.n_epochs + 1):
-            print(f'=============== {epoch}/{self.n_epochs} ===============\n')
-            loss_sum = 0.
-            correct_cnt = 0
-            total_cnt = 0
+            i = 0
+            for data, mask, word_piece_tracked, target in train_dataloader:
+                i += 1
 
-            for data, mask, word_piece_tracked, target in dataloader:
                 data = data.to(self.device)
                 mask = mask.to(self.device)
                 target = target.to(self.device)
 
                 outputs = self.model(data, mask, word_piece_tracked)
-                calculate = get_cross_entropy_loss(outputs, target)
-                loss = calculate[0]
-                loss_sum += loss.item()
-                total_cnt += calculate[1]
-                print(f'training loss = {loss.item()}\n')
-                correct_cnt += (torch.argmax(outputs, dim = 2) == target).sum()
+
+                outputs_ = outputs.view(-1, outputs.size(-1))
+                target_ = target.view(-1)
+                loss = self.criterion(outputs_, target_)
+                print(f'[epoch {epoch}/{self.n_epochs}] averaged training loss of batch {i}/{len(train_dataloader)} = {loss.item()}')
 
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
 
-            loss_sum /= len(dataloader)
-            print(f'======== acc: {(correct_cnt / total_cnt) * 100: .3f} ========\n')
+            print(f'\n======== [epoch {epoch}/{self.n_epochs}] train data evaluation ========\n')
+            self.test(train_dataset, mode = 'train_eval')
+            print(f'\n======== [epoch {epoch}/{self.n_epochs}] dev data evaluation ========\n')
+            self.test(dev_dataset, mode = 'dev_eval')
 
-
-    def test(self, dataset: Dataset):
+    def test(self, dataset: Dataset, mode: str): # mode: ['train_eval', 'dev_eval', 'test_eval']
         dataloader = DataLoader(
             dataset = dataset,
             batch_size = self.batch_size,
-            shuffle = True,
+            shuffle = False,
             collate_fn = collate_fn
         )
 
-        self.model.to(self.device)
+        self.model = self.model.to(self.device)
         self.model.eval()
 
         loss_sum = 0.
         correct_cnt = 0
         total_cnt = 0
 
+        i = 0
         for data, mask, word_piece_tracked, target in dataloader:
+            i += 1
+            if i % 50 == 0:
+                print(f'{mode} progress: {i}/{len(dataloader)}')
+
             data = data.to(self.device)
             mask = mask.to(self.device)
             target = target.to(self.device)
 
             outputs = self.model(data, mask, word_piece_tracked)
-            calculate = get_cross_entropy_loss(outputs, target)
-            loss = calculate[0]
+
+            outputs_ = outputs.view(-1, outputs.size(-1))
+            target_ = target.view(-1)
+            loss = self.criterion(outputs_, target_)
             loss_sum += loss.item()
-            total_cnt += calculate[1]
-            print(f'testing loss = {loss.item()}\n')
+
+            total_cnt += sum(
+                [
+                    sum([1 if cat_id >= 0 else 0 for cat_id in tgt])
+                    for tgt in target
+                ]
+            )
             correct_cnt += (torch.argmax(outputs, dim = 2) == target).sum()
 
         loss_sum /= len(dataloader)
-        print(f'======== acc: {(correct_cnt / total_cnt) * 100: .3f} ========\n')
+        print(f'averaged {mode} loss = {loss_sum}')
+        print(f'{mode} acc = {(correct_cnt / total_cnt) * 100: .3f}')
 
 def main(args):
     auto_parser = AutoParser()
 
-    # train_data_items, categories = auto_parser.load_auto_file(args.train_data_dir)
-    # dev_data_items, categories = auto_parser.load_auto_file(args.dev_data_dir)
+    print('================= parsing data =================\n')
+    train_data_items, categories = auto_parser.load_auto_file(args.train_data_dir)
+    dev_data_items, _ = auto_parser.load_auto_file(args.dev_data_dir)
     # test_data_items, _ = auto_parser.load_auto_file(args.test_data_dir)
-    data_items, categories = auto_parser.load_auto_file(args.sample_data_dir)
 
     category2idx = {categories[idx]: idx for idx in range(len(categories))}
+    category2idx[UNK_CATEGORY] = len(category2idx)
     idx2category = {idx: category for category, idx in category2idx.items()}
 
+    print('================= preparing data =================\n')
     tokenizer = BertTokenizer.from_pretrained(args.model_path)
-    # train_data = prepare_data(train_data_items, tokenizer, category2idx)
-    # dev_data = prepare_data(dev_data_items, tokenizer, category2idx)
+    train_data = prepare_data(train_data_items, tokenizer, category2idx)
+    dev_data = prepare_data(dev_data_items, tokenizer, category2idx)
     # test_data = prepare_data(test_data_items, tokenizer, category2idx)
-    data = prepare_data(data_items, tokenizer, category2idx)
 
-    dataset = CCGSupertaggingDataset(
-        data = data['input_ids'],
-        mask = data['mask'],
-        word_piece_tracked = data['word_piece_tracked'],
-        target = data['target']
+    train_dataset = CCGSupertaggingDataset(
+        data = train_data['input_ids'],
+        mask = train_data['mask'],
+        word_piece_tracked = train_data['word_piece_tracked'],
+        target = train_data['target']
+    )
+    dev_dataset = CCGSupertaggingDataset(
+        data = dev_data['input_ids'],
+        mask = dev_data['mask'],
+        word_piece_tracked = dev_data['word_piece_tracked'],
+        target = dev_data['target']
     )
 
     trainer = CCGSupertaggingTrainer(
@@ -159,11 +180,13 @@ def main(args):
             n_classes = len(category2idx),
             dropout_p = args.dropout_p
         ),
-        batch_size = args.batch_size
+        batch_size = args.batch_size,
+        lr = args.lr
     )
 
-    trainer.train(dataset)
-
+    print('================= supertagging training =================\n')
+    trainer.train(train_dataset = train_dataset, dev_dataset = dev_dataset)
+    # trainer.test(dataset = test_dataset)
     
 
 if __name__ == '__main__':
@@ -171,7 +194,7 @@ if __name__ == '__main__':
     parser.add_argument('--n_epochs', type = int, default = 20)
     parser.add_argument('--device', type = torch.device, default = torch.device('cuda:0'))
     parser.add_argument('--batch_size', type = int, default = 10)
-    parser.add_argument('--lr', type = float, default = 0.001)
+    parser.add_argument('--lr', type = float, default = 1e-5)
     parser.add_argument('--dropout_p', type = float, default = 0.2)
     parser.add_argument('--sample_data_dir', type = str, default = '../data/ccg-sample.auto')
     parser.add_argument('--train_data_dir', type = str, default = '../data/ccgbank-wsj_02-21.auto')
--- ccg_supertagger/utils.py
+++ ccg_supertagger/utils.py
@@ -6,6 +6,10 @@ from typing import List, Tuple, Dict
 sys.path.append('..')
 from data_loader import DataItem
 
+UNK_CATEGORY = 'UNK_CATEGORY'
+DATA_MASK_PADDING = 0
+TARGET_PADDING = -100
+
 def pre_tokenize_sent(sent: str) -> List[str]:
     splited = sent.split(' ')
     returned = list()
@@ -18,7 +22,10 @@ def pre_tokenize_sent(sent: str) -> List[str]:
 
 def get_cat_ids(categories: List[str], category2idx: Dict[str, int]) -> List[int]:
     # take one list of category strings and return a list of corresponding one-hot vectors
-    return [category2idx[category] for category in categories]
+    return [
+        category2idx[category] if category in category2idx.keys() else category2idx[UNK_CATEGORY] 
+        for category in categories
+    ]
 
 def prepare_data(data_items: List[DataItem], tokenizer, category2idx: Dict[str, int]):
     # return wrapped data needed to input into the model.
@@ -51,10 +58,10 @@ def prepare_data(data_items: List[DataItem], tokenizer, category2idx: Dict[str, 
     )
     for i in range(len(data)):
         assert len(data[i]) == len(mask[i])
-        data[i] = data[i] + [0] * (max_length - len(data[i])) # padding
-        mask[i] = mask[i] + [0] * (max_length - len(mask[i])) # padding
+        data[i] = data[i] + [DATA_MASK_PADDING] * (max_length - len(data[i])) # padding
+        mask[i] = mask[i] + [DATA_MASK_PADDING] * (max_length - len(mask[i])) # padding
     for i in range(len(target)):
-        target[i] = target[i] + [-100] * (max_length - len(target[i])) # padding
+        target[i] = target[i] + [TARGET_PADDING] * (max_length - len(target[i])) # padding
 
     return {
         'input_ids': data,
@@ -62,19 +69,6 @@ def prepare_data(data_items: List[DataItem], tokenizer, category2idx: Dict[str, 
         'word_piece_tracked': word_piece_tracked,
         'target': target
     }
-
-def get_cross_entropy_loss(outputs: torch.Tensor, target: torch.Tensor) -> Tuple[torch.Tensor, int]:
-    # outputs: B*L*C, B - batch, L - max_length, C - number of categories
-    # target: B*L, padding value -100
-    loss = 0.0
-    cnt = 0
-    for i in range(outputs.shape[0]):
-        for j in range(outputs.shape[1]):
-            if target[i, j] >= 0:
-                loss += -torch.log(outputs[i, j, target[i, j]])
-                cnt += 1
-    return loss/cnt, cnt
-
 
 
 if __name__ == '__main__':
