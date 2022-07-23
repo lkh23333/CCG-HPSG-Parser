92f61ab5d7924de2b2e13622d3291142c2f6f0ab
attrn <anh.tt@outlook.com>
2022-07-23 21:21:57 +0900

    update data_loader

--- data_loader.py
+++ data_loader.py
@@ -1,3 +1,4 @@
+"""adapted from depccg's implementation @ https://github.com/masashi-y/depccg"""
 from typing import List, Tuple, NamedTuple
 from pyparsing import *
 from base import Token, ConstituentNode, Category
@@ -9,127 +10,145 @@ class DataItem(NamedTuple):
     tree_root: ConstituentNode
 
 
-class AutoParser:
-    # pyparsing patterns
-    integer = pyparsing_common.integer
-    category = Word('SNP()\\/|[].,:;?_BURL' + srange('[0-9]') + srange('[a-z]'))
-    pos = Word(srange('[A-Z]') + '.,-$:;?#non`')
-    word = Word(pyparsing_unicode.printables)
-    angled = oneOf('< >')
-    node_type = oneOf('T L')
+class _AutoLineReader(object):
+    def __init__(self, line):
+        self.line = line
+        self.index = 0
+        self.word_id = -1
+        self.tokens = []
+        self.cats = set()
 
-    # pattern for non-terminal nodes: (<T CCGcat head dtrs> (...) (...))
-    non_leaf = Suppress(angled) + node_type + category + Suppress(integer) + integer + Suppress(angled)
+    def next(self):
+        end = self.line.find(' ', self.index)
+        res = self.line[self.index:end]
+        self.index = end + 1
+        return res
 
-    # pattern for terminal nodes: (<L CCGcat mod_POS-tag orig_POS-tag word PredArgCat>)
-    leaf = Suppress(angled) + node_type + category + pos + Suppress(pos) + word + \
-        SkipTo(category).suppress() + Suppress(category) + Suppress(angled)
+    def check(self, text, offset=0):
+        if self.line[self.index + offset] != text:
+            raise RuntimeError(f'failed to parse: {self.line}')
 
-    parser = OneOrMore(nestedExpr(content=(non_leaf | leaf)))
+    def peek(self):
+        return self.line[self.index]
 
-    def load_auto_file(self, filename: str) -> Tuple[List[DataItem], List[str]]:
-        """
-        load CCG data from .auto file
+    def parse(self):
+        tree = self.next_node()
+        return tree, self.tokens, self.cats
 
-        Args:
-            filename (str): name of .auto file
+    @property
+    def next_node(self):
+        if self.line[self.index + 2] == 'L':
+            return self.parse_leaf
+        elif self.line[self.index + 2] == 'T':
+            return self.parse_tree
+        else:
+            raise RuntimeError(f'failed to parse: {self.line}')
 
-        Returns:
-            Tuple[List[DataItem], List[str]]: a tuple that contains:
-                - a list of CCG trees, each corresponding to a sentence
-                - a list of all unique categories in the file
-        """
+    def parse_leaf(self):
+        self.word_id += 1
+        self.check('(')
+        self.check('<', 1)
+        self.check('L', 2)
+        self.next()
 
-        # referenced from depccg's implementation
-        # fix unwanted categories such as (S\\NP)\\(S\\NP)[conj]
-        __fix = {'((S[b]\\NP)/NP)/': '(S[b]\\NP)/NP', 'conj[conj]': 'conj'}
+        cat_str = self.next()
+        cat = Category.parse(cat_str)
+        self.cats.add(cat_str)
 
-        def _fix(cat):
-            if cat in __fix:
-                return __fix[cat]
-            if cat.endswith(')[conj]') or cat.endswith('][conj]'):
-                return cat[:-6]
-            return cat
+        tag1 = self.next()  # modified POS tag
+        tag2 = self.next()  # original POS
+        word = self.next().replace('\\', '')
 
-        with open(filename, 'r') as f:
-            lines = f.readlines()
+        token = Token(
+            contents=word,
+            POS=tag1,
+            tag=cat
+        )
+        self.tokens.append(token)
 
-            data_items = list()
-            all_cats = set()
+        if word == '-LRB-':
+            word = "("
+        elif word == '-RRB-':
+            word = ')'
+        self.next()
 
-            for line in lines:
-                line = line.strip()
-                line = ' '.join(_fix(token) for token in line.split(' '))
+        return token
 
-                if len(line) == 0:
-                    continue
-                if line.startswith("ID"):
-                    id = line
-                else:
-                    tokens = list()
+    def parse_tree(self):
+        self.check('(')
+        self.check('<', 1)
+        self.check('T', 2)
+        self.next()
 
-                    try:
-                        parsed = AutoParser.parser.parseString(line).asList()
-                    except ParseException as e:
-                        print('pyparsing error: ' + repr(e))     # TODO: use a logger at some point
-                        # raise
-                        continue
+        cat_str = self.next()
+        cat = Category.parse(cat_str)
+        self.cats.add(cat_str)
 
-                    if parsed is None or len(parsed) == 0 or len(parsed[0]) == 0:
-                        continue
-                    elif parsed[0][0] == 'L':     # if leaf node
-                        parsed = parsed[0]
-                        category = parsed[1]
-                        pos = parsed[2]
-                        word = parsed[3]
+        head_is_left = self.next() == '0'
+        self.next()
+        children = []
+        while self.peek() != ')':
+            children.append(self.next_node())
+        self.next()
 
-                        token = Token(contents=word, POS=pos, tag=Category.parse(category))
-                        tokens.append(token)
-                        all_cats.add(category)
-                    else:                         # if non-leaf node
-                        parsed = parsed[0]
+        if len(children) > 2:
+            raise RuntimeError(f'failed to parse: {self.line}')
+        else:
+            node = ConstituentNode(tag=cat)
+            node.children = children
+            return node
 
-                        def _traverse(res):
-                            if res is not None and len(res) > 0:
-                                if res[0] == 'L':
-                                    category = res[1]
-                                    pos = res[2]
-                                    word = res[3]
 
-                                    token = Token(contents=word, POS=pos, tag=Category.parse(category))
-                                    tokens.append(token)
-                                    all_cats.add(category)
+def load_auto_file(filename: str) -> Tuple[List[DataItem], List[str]]:
+    """read traditional AUTO file used for CCGBank
+    English CCGbank contains some unwanted categories such as (S\\NP)\\(S\\NP)[conj].
+    This reads the treebank while taking care of those categories.
 
-                                    return token
-                                else:
-                                    category = res[1]
-                                    num_children = res[2]
+    Args:
+        filename (str): file name string
 
-                                    node = ConstituentNode(tag=Category.parse(category))
-                                    all_cats.add(category)
+    Yields:
+        Iterator[ReaderResult]: iterator object containing parse results
+    """
 
-                                    if num_children == 1:
-                                        children = [_traverse(res[3])]
-                                    else:
-                                        children = [_traverse(res[3]), _traverse(res[4])]
+    __fix = {'((S[b]\\NP)/NP)/': '(S[b]\\NP)/NP', 'conj[conj]': 'conj'}
 
-                                    node.children = children
+    def _fix(cat):
+        if cat in __fix:
+            return __fix[cat]
+        if cat.endswith(')[conj]') or cat.endswith('][conj]'):
+            return cat[:-6]
+        return cat
 
-                                    return node
+    data_items = list()
+    all_cats = set()
 
-                        root = _traverse(parsed)
+    with open(filename, 'r') as f:
+        lines = f.readlines()
 
-                        data_items.append(DataItem(id, tokens, root))
+        for line in lines:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            if line.startswith("ID"):
+                id = line
+            else:
+                line = ' '.join(
+                    _fix(token) for token in line.split(' ')
+                )
+                root, tokens, cats = _AutoLineReader(line).parse()
 
-            return data_items, list(all_cats)
+                data_items.append(DataItem(id, tokens, root))
+                all_cats.union(cats)
+
+    return data_items, list(all_cats)
 
 
 if __name__ == '__main__':
     # sample usage
     filename = "data/ccg-sample.auto"
 
-    auto_parser = AutoParser()
-    items, cats = auto_parser.load_auto_file(filename)
+    items, cats = load_auto_file(filename)
 
     for item in items:
         print(item.id)
