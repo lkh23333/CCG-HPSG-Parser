69bfe9d63959a44491631064a3798de808cd95dc
lkh23333 <1900011070@pku.edu.cn>
2022-07-24 04:14:03 -0400

    update

--- ccg_supertagger/models/simple_model.py
+++ ccg_supertagger/models/simple_model.py
@@ -1,9 +1,27 @@
 from typing import List
+import os, random
 import numpy as np
 import torch
 import torch.nn as nn
 from transformers import BertModel
 
+# to set the random seeds
+def _setup_seed(seed):
+    np.random.seed(seed)
+    random.seed(seed)
+
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
+
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    
+    torch.use_deterministic_algorithms(True)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.enabled = False
+    torch.backends.cudnn.benchmark = False
+_setup_seed(0)
 
 class CCGSupertaggerModel(nn.Module):
     def __init__(
@@ -25,21 +43,23 @@ class CCGSupertaggerModel(nn.Module):
             input_ids = encoded_batch,
             attention_mask = mask
         ).last_hidden_state # B*L*H
-        f1 = self.w1(
-            self.dropout(
-                self.relu(f0)
-            )
-        )
-        f2 = self.w2(
-            self.dropout(
-                self.relu(f1)
-            )
-        )
-        for i in range(f2.shape[0]):
+
+        for i in range(f0.shape[0]):
             k = 0
             for j in range(len(word_piece_tracked[i])):
                 n_piece = word_piece_tracked[i][j]
-                f2[i, j] = torch.sum(f2[i, k:k+n_piece], dim = 0) / n_piece # to take the average of word pieces
+                f0[i, j] = torch.sum(f0[i, k:k+n_piece], dim = 0) / n_piece # to take the average of word pieces
                 k += n_piece
+        
+        f1 = self.dropout(
+            self.relu(
+                self.w1(f0)
+            )
+        )
+        f2 = self.dropout(
+            self.relu(
+                self.w2(f1)
+            )
+        )
 
-        return self.softmax(f2) # B*L*C (C the number of classes)
+        return f2 # B*L*C (C the number of classes)
